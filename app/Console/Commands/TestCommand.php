a07cd204b5bf76b0bccd2b938e091524ad1aba97
totti <576768947@qq.com>
2018-07-19 11:41:37 +0800

    201807191141

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -8,6 +8,11 @@ use App\Models\OrderGoods;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
+
+//sh -c "cd /www/wwwroot/laravel_5.4.36;php artisan command:test;"
+//0 */1 * * * /home/crontab/laravel_5.4.36.sh
+
+
 class TestCommand extends Command
 {
     /**
@@ -43,10 +48,15 @@ class TestCommand extends Command
     {
         //执行的逻辑
         try {
-            $where['orderstatus']=1;
-            $condition['addtime']=['<',time() - 72 * 3600];
 
-            $order_ids = Order::where($where)->where('addtime','<',time() - 72 * 3600)->select('id')->get();
+            $where=[
+                ['orderstatus',1],
+                ['addtime','<',time()-72*3600]
+            ];
+
+            $order_ids = Order::where($where)->select('id')->get();
+
+            //$order_ids = Order::where($where)->where('addtime','<',time() - 72 * 3600)->select('id')->get();
 
             $list = OrderGoods::when($order_ids, function ($query) use ($order_ids) {
                 $query->whereIn('oid', $order_ids);
@@ -59,7 +69,7 @@ class TestCommand extends Command
                     Goods::where('id', $v['gid'])->increment('num', $v['buynum']);
 
                     $goods = Goods::find($v['gid']);
-                    if ($goods['salenum'] >= $v['buynum']) {
+                    if (intval($goods['salenum']) >= intval($v['buynum'])) {
                         Goods::where('id', $v['gid'])->decrement('salenum', $v['buynum']);
                     } else {
                         Goods::where('id', $v['gid'])->update(['salenum' => 0]);
@@ -67,11 +77,14 @@ class TestCommand extends Command
                 }
             }
 
-            $row=Order::where($where)->where('addtime','<',time() - 72 * 3600)->update(['orderstatus' => 9]);
+            $row=Order::where($where)->update(['orderstatus' => 9]);
+            if ($row) {
+                Log::info('command:success_' . time() . '_条数：' . $row);
+            } else {
+                Log::info('command:error_' . time());
+            }
         }catch (\Exception $e){
-            Log::info('command:'.$e->getMessage());
+            Log::info('command:warning_'.time().'_'.$e->getMessage());
         }
-
-        Log::info('command:success'.$row);
     }
 }
