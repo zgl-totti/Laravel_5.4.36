beab5f23d9cc97f7739ed571779e0749901c7c14
zgl <724249769@qq.com>
2019-07-11 22:10:14 +0800

    蜘蛛添加

--- app/Http/Middleware/LoginMiddleware.php
+++ app/Http/Middleware/LoginMiddleware.php
@@ -8,15 +8,20 @@
 
 namespace App\Http\Middleware;
 
-
+use App\Services\Spider;
 use Illuminate\Http\Request;
 
-class LoginMiddleware {
-    public function handle(Request $request,\Closure $next){
-        $aid=$request->session()->get('aid');
-        if(is_int($aid) && $aid>0){
+class LoginMiddleware
+{
+    public function handle(Request $request, \Closure $next)
+    {
+        //蜘蛛来访存入
+        Spider::getInstance()->store();
+
+        $aid = $request->session()->get('aid');
+        if (is_int($aid) && $aid > 0) {
             return $next($request);
-        }else{
+        } else {
             return redirect('https://www.baidu.com');
         }
     }
