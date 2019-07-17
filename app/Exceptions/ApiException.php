cd7c88b9f3c270eeddeaaee6bc478c85248211d0
zgl <724249769@qq.com>
2019-07-18 06:48:25 +0800

    优化

--- app/Exceptions/ApiException.php
+++ app/Exceptions/ApiException.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
 
 class ApiException extends Exception
 {
@@ -17,5 +19,28 @@ class ApiException extends Exception
         $this->code=0;
 
         $this->http_code=$http_code;
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if($request->expectsJson()){
+            $response=response()->json([
+                'status'=>3,
+                'msg' => $exception->getMessage(),
+                'errors'=>[],
+            ], 200);
+        }else{
+            $response=redirect()->guest(route('login'));
+        }
+        return $response;
+    }
+
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        return response()->json([
+            'status'=>2,
+            'msg' => $exception->getMessage(),
+            'errors' => $exception->errors(),
+        ], $exception->status);
     }
 }
